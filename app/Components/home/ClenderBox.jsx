6e6e89045f4beece1075bc1d3bb440c7e3a2c1fa
iamamansinghrajput <singhaman2321@gmail.com>
2025-08-09 01:05:27 +0530

    calander

--- app/Components/home/ClenderBox.jsx
+++ app/Components/home/ClenderBox.jsx
@@ -1,11 +1,24 @@
 import { View, Text } from 'react-native';
 
-export default function ClenderBox({ data }) {
+export default function ClenderBox({ data, isToday }) {
   const { date, day } = data;
+
   return (
-    <View className="w-20 h-20 bg-white rounded-lg items-center justify-center p-2 border border-gray-200">
-      <Text className="text-2xl font-bold text-black">{date}</Text>
-      <Text className="text-base text-gray-500">{day}</Text>
+    <View
+      className={`w-20 h-20 rounded-lg items-center justify-center p-2 border 
+        ${isToday ? 'bg-[#2563eb] border-[#2563eb]' : 'bg-white border-gray-200'}
+      `}
+    >
+      <Text
+        className={`text-2xl font-bold ${isToday ? 'text-white' : 'text-black'}`}
+      >
+        {date}
+      </Text>
+      <Text
+        className={`text-base ${isToday ? 'text-white' : 'text-gray-500'}`}
+      >
+        {day}
+      </Text>
     </View>
   );
 }
